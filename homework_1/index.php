c766e41f42df5a6f7184b3ebe255e6c99b32bc26
adilya-g <uitaci099@gmail.com>
2026-02-16 18:21:48 +0300

    some_fixes

--- homework_1/index.php
+++ homework_1/index.php
@@ -1,10 +1,16 @@
 <?php
 $name = htmlspecialchars($_GET['name'], ENT_QUOTES | ENT_SUBSTITUTE, 'UTF-8');
 $role = htmlspecialchars($_GET['role'], ENT_QUOTES | ENT_SUBSTITUTE, 'UTF-8');
+$protocol = (!empty($_SERVER['HTTPS']) && $_SERVER['HTTPS'] !== 'off' || $_SERVER['SERVER_PORT'] == 443) ? "https://" : "http://";
+$host = $_SERVER['HTTP_HOST'];
+$uri = $_SERVER['REQUEST_URI'];
+$full_url = $protocol . $host . $uri;
+
 if($role == 'admin'){
-    echo "<h1>Добрый день, админ $name</h1>";
+    echo "<h1>Добрый день, админ $name</h1></br>";
 }
 else{
-    echo "<h1>Добрый день, $name</h1>";
+    echo "<h1>Добрый день, $name</h1></br>";
 }
+echo $full_url;
 ?>
